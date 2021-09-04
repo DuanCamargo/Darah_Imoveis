b4e0b7732bb51643d9ae385c9461330e79f5bfd1
Andrei <andrei09ka6@gmail.com>
2021-09-04 11:39:24 -0300

    teste

--- src/Components/FormImovel/index.js
+++ src/Components/FormImovel/index.js
@@ -5,15 +5,11 @@ import { Link } from 'react-router-dom';
 import { PostFormImov} from '../../Service/PostImovelForm'
 
 
-<<<<<<< HEAD
 
 // import{ useHistory} from 'react-router-dom
 
 
 const FormImovel = ()=>{
-=======
-const FormImovel = ({alt}) => {
->>>>>>> 3d7b944a75620dd27f1e7d9ca45f9dd33ba27c5d
 
      const initialCadastroState = {
          cep: "",
@@ -43,52 +39,6 @@ const FormImovel = ({alt}) => {
 
     return (
         <>
-<<<<<<< HEAD
-           
-           
-        <FormContainerGeral>
-        
-       
-       <FormContainer>
-           
-       <Form> <RiAccountCircleFill/> Faça o seu Anúncio</Form>
-           <form onSubmit={SendForm} >
-                <div className="form-group">
-                    <input type="number" className="form-control" id="cep"  name="cep" placeholder="Cep" onChange={handleInputChange} required />  
-                </div>
-
-                <div className="form-group">
-                    <input type="text" className="form-control" id="logradouro" name="logradouro" placeholder="Logradouro" onChange={handleInputChange} required/>                  
-                </div>
-
-                <div className="form-group">
-                    <input type="text" className="form-control" id="numero" name="numero" placeholder="Nº" onChange={handleInputChange} required/>
-                </div>
-
-                <div className="form-group">
-                    <input type="text" className="form-control" id="complemento" name="complemento"  placeholder="Complemento" onChange={handleInputChange} required />        
-                </div>
-
-                <div className="form-group">
-                    <input type="text" className="form-control" name="cidade" id="cidade" placeholder="Cidade" onChange={handleInputChange} required/>
-                </div>
-
-                <div className="form-group">
-                    <input type="text" className="form-control" name="estado" id="estado" placeholder="UF" onChange={handleInputChange} required/>
-                </div>
-
-                <div className="col text-center">
-                    <button type="submit" className="btn btn-primary ">Próximo</button>
-                </div>
-             
-            </form>
-
-            
-            
-       </FormContainer>
-
-       
-=======
                 
         <FormContainerGeral> 
             <FormContainer>
@@ -127,7 +77,6 @@ const FormImovel = ({alt}) => {
                     </div>
                 </form>
             </FormContainer>
->>>>>>> 3d7b944a75620dd27f1e7d9ca45f9dd33ba27c5d
        </FormContainerGeral>  
         </>
     )
