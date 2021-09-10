6552e4be3efca016ae9c0507019fc5b162de6b87
Duan Camargo <duan1505@gmail.com>
2021-09-10 14:36:10 -0300

    update

--- src/Components/FormImovel/CadAnuncio/index.js
+++ src/Components/FormImovel/CadAnuncio/index.js
@@ -1,118 +1,186 @@
-import React, { useEffect } from 'react';
-import { FormContainerGeral, FormContainer, FormIM2, FormContainerFotoG, FormContainerFoto } from './CadAnuncioStyle';
-import { useState} from 'react';
-import { Button } from '../../SectionHome/SectionStyle';
-import { PostCadImovel } from '../../../Service/PostCadImovel';
-import { useLocation } from 'react-router';
+import React, { useEffect } from "react";
+import {
+  FormContainerGeral,
+  FormContainer,
+  FormIM2,
+  FormContainerFotoG,
+  FormContainerFoto,
+} from "./CadAnuncioStyle";
+import { useState } from "react";
+import { Button } from "../../SectionHome/SectionStyle";
+import { PostCadImovel } from "../../../Service/PostCadImovel";
+import { useLocation } from "react-router";
 
 const CadAnuncio = () => {
+  const init = {
+    tipo_compartilhamento: "",
+    detalhes_residencia: "",
+    regras_residencia: "",
+    detalhes_regiao: "",
+    detalhes_quarto: "",
+    mapa_embed: "",
 
-    const init = {
-        tipo_compartilhamento: "",
-        detalhes_residencia: "",
-        regras_residencia: "",
-        detalhes_regiao: "",
-        detalhes_quarto: "",
-        mapa_embed: "",
-        
-        imovelDTO:{
-            id_imovel: 0,
-        }
-        
-        
+    imovelDTO: {
+      id_imovel: 0,
+    },
+  };
+
+  const location = useLocation();
+  const [input, setInput] = useState(init);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInput({
+      ...input,
+      [name]: value,
+      data_publicacao: dataEHora,
+      imovelDTO: { id_imovel: location.state },
+    });
+  };
+
+  console.log(input);
+
+  const SendResidencia = (e) => {
+    e.preventDefault();
+    PostCadImovel(input);
+  };
+
+  const dateTime = (data = new Date()) => {
+    var dia = data.getDate();
+    var mes = data.getMonth() + 1;
+    var ano = data.getFullYear();
+    var hora = data.getHours();
+    var minute = data.getMinutes();
+    var second = data.getSeconds();
+
+    if (dia.toString().length === 1) {
+      dia = "0" + dia;
     }
-
-    const location = useLocation()
-    const [input, setInput] = useState(init)
-    
-
-
-    const handleInputChange = (event) =>{
-        const {name, value} = event.target;
-        setInput({ ...input, [name]: value, data_publicacao: dataEHora, imovelDTO:{id_imovel: location.state}});
+    if (mes.toString().length === 1) {
+      mes = "0" + mes;
     }
-
-    console.log(input)
-
-    const SendResidencia = (e) => {
-        e.preventDefault();
-        PostCadImovel(input);
+    if (hora.toString().length === 1) {
+      hora = "0" + hora;
     }
-
-    const dateTime = (data = new Date()) =>{
-        var dia = data.getDate();
-        var mes = data.getMonth()+1;
-        var ano = data.getFullYear(); 
-        var hora = data.getHours();
-        var minute = data.getMinutes();
-        var second = data.getSeconds()
- 
-        if(dia.toString().length ===1){dia = '0'+dia}
-        if(mes.toString().length ===1){mes = '0'+mes}
-        if(hora.toString().length ===1){hora = '0'+hora}
-        if(minute.toString().length ===1){minute = '0'+minute}
-        if(second.toString().length ===1){second = '0'+second}
-    
-        return ano+'-'+mes+'-'+dia+' '+hora+':'+minute+':'+second;
+    if (minute.toString().length === 1) {
+      minute = "0" + minute;
     }
-    var dataEHora =  dateTime()
+    if (second.toString().length === 1) {
+      second = "0" + second;
+    }
 
     return (
-        <>
-            <FormContainerGeral>
-                <FormContainer id="residencia">
-                    <form onSubmit={SendResidencia}>
-                        <FormIM2>Preencha os campos abaixo</FormIM2>
-                        <div>
-                            <label htmlFor="tipo_compartilhamento">Tipo de Compartilhamento</label>
-                            <select id="tipo_compartilhamento" name="tipo_compartilhamento" onChange={handleInputChange}>
-                                <option value={null}>Selecione uma opção abaixo</option>
-                                <option value="quarto_residencia">Quarto e residência</option>
-                                <option value="residencia">Residência</option>
-                            </select>
-                        </div>
+      ano + "-" + mes + "-" + dia + " " + hora + ":" + minute + ":" + second
+    );
+  };
+  var dataEHora = dateTime();
 
-                        <div>
-                            <label htmlFor="detalhes_residencia">Detalhes da Residência</label>
-                            <textarea className="form-control" id="detalhes_residencia" name="detalhes_residencia" placeholder="ex: Ar-Condicionado, Sugar, " onChange={handleInputChange} />
-                        </div>
+  return (
+    <>
+      <FormContainerGeral>
+        <FormContainer id="residencia">
+          <form onSubmit={SendResidencia}>
+            <FormIM2>Preencha os campos abaixo</FormIM2>
+            <div>
+              <label htmlFor="tipo_compartilhamento">
+                Tipo de Compartilhamento
+              </label>
+              <select
+                id="tipo_compartilhamento"
+                name="tipo_compartilhamento"
+                onChange={handleInputChange}
+              >
+                <option value={null}>Selecione uma opção abaixo</option>
+                <option value="quarto_residencia">Quarto e residência</option>
+                <option value="residencia">Residência</option>
+              </select>
+            </div>
 
-                        <div>
-                            <label htmlFor="regras_residencia">Regras da Residência</label>
-                            <textarea className="form-control" id="regras_residencia" name="regras_residencia" placeholder="Ex:  não pode festa, não pode pet, etc..." onChange={handleInputChange} />
-                        </div>
+            <div>
+              <label htmlFor="detalhes_residencia">
+                Detalhes da Residência
+              </label>
+              <textarea
+                className="form-control"
+                id="detalhes_residencia"
+                name="detalhes_residencia"
+                placeholder="ex: Ar-Condicionado, Sugar, "
+                onChange={handleInputChange}
+              />
+            </div>
 
-                        <div>
-                            <label htmlFor="detalhes_regiao">Detalhes da Região</label>
-                            <textarea className="form-control" id="detalhes_regiao" name="detalhes_regiao" placeholder="ex: Supermercado, farmacia, etc... " onChange={handleInputChange} />
-                        </div>
+            <div>
+              <label htmlFor="regras_residencia">Regras da Residência</label>
+              <textarea
+                className="form-control"
+                id="regras_residencia"
+                name="regras_residencia"
+                placeholder="Ex:  não pode festa, não pode pet, etc..."
+                onChange={handleInputChange}
+              />
+            </div>
 
-                        <div>
-                            <label htmlFor="detalhes_quarto">Detalhes Quarto</label>
-                            <textarea type="text" className="form-control" id="detalhes_quarto" name="detalhes_quarto" placeholder="ex: mobiliado, frigobar, ar-condicionado, etc" onChange={handleInputChange} />
-                        </div>
+            <div>
+              <label htmlFor="detalhes_regiao">Detalhes da Região</label>
+              <textarea
+                className="form-control"
+                id="detalhes_regiao"
+                name="detalhes_regiao"
+                placeholder="ex: Supermercado, farmacia, etc... "
+                onChange={handleInputChange}
+              />
+            </div>
 
-                        {/* ########### MAPA ######### */}
-                        <div>
-                            <label htmlFor="mapa_embed">Mapa (EMBED)</label>
-                            <input type="text" className="form-control" id="mapa_embed" name="mapa_embed" placeholder="Embed seu mapa aqui" onChange={handleInputChange} />
-                        </div>
+            <div>
+              <label htmlFor="detalhes_quarto">Detalhes Quarto</label>
+              <textarea
+                type="text"
+                className="form-control"
+                id="detalhes_quarto"
+                name="detalhes_quarto"
+                placeholder="ex: mobiliado, frigobar, ar-condicionado, etc"
+                onChange={handleInputChange}
+              />
+            </div>
 
-                        <div>
-                            <input type="datetime" defaultValue={dataEHora} className="form-control" id="data_publicacao" name="data_publicacao" hidden/>
-                        </div>
+            {/* ########### MAPA ######### */}
+            <div>
+              <label htmlFor="mapa_embed">Mapa (EMBED)</label>
+              <input
+                type="text"
+                className="form-control"
+                id="mapa_embed"
+                name="mapa_embed"
+                placeholder="Embed seu mapa aqui"
+                onChange={handleInputChange}
+              />
+            </div>
 
-                        <div className="col text-center">
-                            <Button type="submit" className="btn btn-primary ">Próximo</Button>
-                        </div>
-                        <div className="col text-center">
-                            <Button type="submit" className="btn btn-danger ">Voltar</Button>
-                        </div>
-                    </form>
-                </FormContainer>
-            </FormContainerGeral>
-        </>
-    )
-}
-export default CadAnuncio
+            <div>
+              <input
+                type="datetime"
+                defaultValue={dataEHora}
+                className="form-control"
+                id="data_publicacao"
+                name="data_publicacao"
+                hidden
+              />
+            </div>
 
+            <div className="col text-center">
+              <Button type="submit" className="btn btn-primary ">
+                Próximo
+              </Button>
+            </div>
+            <div className="col text-center">
+              <Button type="submit" className="btn btn-danger ">
+                Voltar
+              </Button>
+            </div>
+          </form>
+        </FormContainer>
+      </FormContainerGeral>
+    </>
+  );
+};
+export default CadAnuncio;
