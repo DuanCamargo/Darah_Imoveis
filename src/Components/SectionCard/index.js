5f078e0ba27fa3add7b3eafa8d384b5bffa00a6c
Henrique <RickAvles@gmail.com>
2021-09-14 15:59:50 -0300

    Pequenas mudanças da home

--- src/Components/SectionCard/index.js
+++ src/Components/SectionCard/index.js
@@ -19,43 +19,73 @@ const SectionCard = () => {
                     <InfoP>Veja abaixo informações sobre anúncios proximo a sua localidade</InfoP>
                
                 <CardContainer>
-                
                     <CardInfo>
-                        <NomeIcon>House type</NomeIcon>
                         <Icon src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__480.jpg"/>
-                        <IconPrice>R$400</IconPrice>
-                        <NomeIcon>Type of room</NomeIcon>
+                        <IconPrice>R$385</IconPrice>
+                        <NomeIcon>Quarto</NomeIcon>
+                        <LocationIcon>Comercio</LocationIcon>
+                        <ButtonIcon>View details</ButtonIcon>
+                    </CardInfo>
+
+                    <CardInfo>
+                        <Icon src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__480.jpg"/>
+                        <IconPrice>R$800</IconPrice>
+                        <NomeIcon>Quarto</NomeIcon>
+                        <LocationIcon>Cabula</LocationIcon>
+                        <ButtonIcon>View details</ButtonIcon>
+                    </CardInfo>
+
+                    <CardInfo>
+                        <Icon src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__480.jpg"/>
+                        <IconPrice>R$450</IconPrice>
+                        <NomeIcon>Residência</NomeIcon>
                         <LocationIcon>Pituba</LocationIcon>
                         <ButtonIcon>View details</ButtonIcon>
                     </CardInfo>
 
                     <CardInfo>
-                        <NomeIcon>House type</NomeIcon>
                         <Icon src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__480.jpg"/>
-                        <IconPrice>R$400</IconPrice>
-                        <NomeIcon>Type of room</NomeIcon>
-                        <LocationIcon>Pituba</LocationIcon>
-                        <ButtonIcon>View details</ButtonIcon>
-                    </CardInfo>
-
-                    <CardInfo>
-                        <NomeIcon>House type</NomeIcon>
-                        <Icon src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__480.jpg"/>
-                        <IconPrice>R$400</IconPrice>
-                        <NomeIcon>Type of room</NomeIcon>
-                        <LocationIcon>Pituba</LocationIcon>
-                        <ButtonIcon>View details</ButtonIcon>
-                    </CardInfo>
-
-                    <CardInfo>
-                        <NomeIcon>House type</NomeIcon>
-                        <Icon src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__480.jpg"/>
-                        <IconPrice>R$400</IconPrice>
-                        <NomeIcon>Type of room</NomeIcon>
-                        <LocationIcon>Pituba</LocationIcon>
+                        <IconPrice>R$630</IconPrice>
+                        <NomeIcon>Quarto</NomeIcon>
+                        <LocationIcon>Cajazeiras</LocationIcon>
                         <ButtonIcon>View details</ButtonIcon>
                     </CardInfo>
                 
+                </CardContainer>
+
+                <CardContainer>
+                
+                    <CardInfo>
+                        <Icon src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__480.jpg"/>
+                        <IconPrice>R$812</IconPrice>
+                        <NomeIcon>Residência</NomeIcon>
+                        <LocationIcon>Brotas</LocationIcon>
+                        <ButtonIcon>View details</ButtonIcon>
+                    </CardInfo>
+
+                    <CardInfo>
+                        <Icon src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__480.jpg"/>
+                        <IconPrice>R$650</IconPrice>
+                        <NomeIcon>Quarto</NomeIcon>
+                        <LocationIcon>Lobato</LocationIcon>
+                        <ButtonIcon>View details</ButtonIcon>
+                    </CardInfo>
+
+                    <CardInfo>
+                        <Icon src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__480.jpg"/>
+                        <IconPrice>R$500</IconPrice>
+                        <NomeIcon>Residência</NomeIcon>
+                        <LocationIcon>Barra</LocationIcon>
+                        <ButtonIcon>View details</ButtonIcon>
+                    </CardInfo>
+
+                    <CardInfo>
+                        <Icon src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__480.jpg"/>
+                        <IconPrice>R$580</IconPrice>
+                        <NomeIcon>Residência</NomeIcon>
+                        <LocationIcon>São Rafael</LocationIcon>
+                        <ButtonIcon>View details</ButtonIcon>
+                    </CardInfo>
                 
                 </CardContainer>
 
